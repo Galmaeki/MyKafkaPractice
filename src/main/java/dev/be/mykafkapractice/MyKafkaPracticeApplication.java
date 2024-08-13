cf0d83ec8467fcf49ec5494d34f14046b720b94e
Galmaeki <kjh09800@gmail.com>
2024-08-13 23:14:09 +0900

    초기화 커밋

---
+++ src/main/java/dev/be/mykafkapractice/MyKafkaPracticeApplication.java
@@ -0,0 +1,13 @@
+package dev.be.mykafkapractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyKafkaPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyKafkaPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/be/mykafkapractice/MyKafkaPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.be.mykafkapractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyKafkaPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
